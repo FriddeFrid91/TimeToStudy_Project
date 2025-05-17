58c96a63184f1a1cbf84c5b233f63326c0dfc58f
Mi <ngoc.hong0001@stud.hkr.se>
2025-05-17 20:04:15 +0200

    schedule.js updated cors row 56 & 75 (icsFilePAth)

--- TimeToStudy/server/schedule.js
+++ TimeToStudy/server/schedule.js
@@ -53,7 +53,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors('*', corsOptions));
+app.use(cors(corsOptions)); //origin: app.use(cors('*', corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -71,7 +71,8 @@ app.get('/api/ics', (req, res) => {
     return res.status(400).json({ error: 'No file specified' });
   }
 
-  const icsFilePath = path.join(__dirname, fileName);
+  const safeFileName = path.basename(fileName); // Strips dangerous paths
+  const icsFilePath = path.join(__dirname, '../schedules', safeFileName);
 
   fs.readFile(icsFilePath, 'utf8', (err, data) => {
     if (err) {
